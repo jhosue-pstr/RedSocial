7e98bed14272aafbe789b213b2e77b91a7bd9389
jhosue-pstr <pastor.jhosue07@gmail.com>
2025-10-09 13:26:43 -0500

    login terminado

--- frontendrs/lib/presentation/screens/login_screen.dart
+++ frontendrs/lib/presentation/screens/login_screen.dart
@@ -89,9 +89,7 @@ class _LoginScreenState extends State<LoginScreen> {
                           if (value == null || value.isEmpty) {
                             return 'Por favor ingresa tu contraseña';
                           }
-                          if (value.length < 6) {
-                            return 'La contraseña debe tener al menos 6 caracteres';
-                          }
+
                           return null;
                         },
                       ),
